4e84ef78cb0b66ce8788551cc4947ca280746baa
manhar <manharchaurasia100@gmail.com>
2026-05-25 15:24:15 +0530

    inline main test

--- src/main.jsx
+++ src/main.jsx
@@ -2,39 +2,6 @@ import React from "react";
 
 import ReactDOM from "react-dom/client";
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import App from "./App";
-
-function OAuthSuccess() {
-  return (
-    <div>
-      <h1>HELLLLLLOO</h1>
-    </div>
-  );
-}
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/workflow/:id" element={<App />} />
-      <Route
-        path="/oauth-success"
-        element={(() => {
-          const params = new URLSearchParams(window.location.search);
-
-          const userId = params.get("userId");
-
-          if (userId) {
-            localStorage.setItem("hubsimplify_user_id", userId);
-          }
-
-          return <h1>HELLLLLLOO</h1>;
-        })()}
-      />{" "}
-    </Routes>
-  </BrowserRouter>,
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <h1>TEST WORKING</h1>,
 );
