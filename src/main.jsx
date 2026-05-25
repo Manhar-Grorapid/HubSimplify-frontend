eff1cf12016492b9028e31131468f11d0a49ce34
manhar <manharchaurasia100@gmail.com>
2026-05-25 12:49:33 +0530

    added successpage import url

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import App from "./App";
+import OAuthSuccess from "./pages/OAuthSuccess";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
