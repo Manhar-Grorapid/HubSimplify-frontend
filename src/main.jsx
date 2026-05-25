9c896bb0f1c6fae030fd853284401ab2e67f1385
manhar <manharchaurasia100@gmail.com>
2026-05-25 13:08:28 +0530

    added comp

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
+import OAuthSuccess from "./pages/OAuthSuccess";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -11,7 +12,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
         <Route path="/" element={<App />} />
 
         <Route path="/workflow/:id" element={<App />} />
-        <Route path="/oauth-success" element={<h1>TEST SUCCESS PAGE</h1>} />
+        <Route path="/oauth-success" element={<OAuthSuccess />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
