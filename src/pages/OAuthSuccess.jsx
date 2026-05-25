8e38ec9d218468c9fdc3df4be88635d2543b0c2b
manhar <manharchaurasia100@gmail.com>
2026-05-25 13:25:14 +0530

    oauth success page

--- src/pages/OAuthSuccess.jsx
+++ src/pages/OAuthSuccess.jsx
@@ -1,18 +1,6 @@
 import { useEffect } from "react";
 
 export default function OAuthSuccess() {
-  useEffect(() => {
-    const params = new URLSearchParams(window.location.search);
-
-    const userId = params.get("userId");
-
-    if (userId) {
-      localStorage.setItem("hubsimplify_user_id", userId);
-
-      console.log("USER ID STORED:", userId);
-    }
-  }, []);
-
   return (
     <div>
       <h1>OAuth Success Working</h1>
