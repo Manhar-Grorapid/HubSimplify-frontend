88a729e59e4907c9a7cae6f2afdc95baff6f85b2
manhar <manharchaurasia100@gmail.com>
2026-05-25 17:03:42 +0530

    added .jsx ext

--- src/pages/OAuthSuccess.jsx
+++ src/pages/OAuthSuccess.jsx
@@ -3,7 +3,7 @@ import { useEffect } from "react";
 export default function OAuthSuccess() {
   return (
     <div>
-      <h1>OAuth Success Working</h1>
+      <h1>OAuth Success Working.......</h1>
     </div>
   );
 }
