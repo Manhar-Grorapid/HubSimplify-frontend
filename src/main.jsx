422122ba732ae88b3fd3a5358161be511ad5574d
manhar <manharchaurasia100@gmail.com>
2026-05-25 13:28:30 +0530

    oauth success page

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
+
 import ReactDOM from "react-dom/client";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import App from "./App";
 
 function OAuthSuccess() {
-  return <h1>INLINE COMPONENT WORKING</h1>;
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+
+    const userId = params.get("userId");
+
+    if (userId) {
+      localStorage.setItem("hubsimplify_user_id", userId);
+
+      console.log("USER ID STORED:", userId);
+    }
+  }, []);
+
+  return (
+    <div>
+      <h1>HubSpot Connected Successfully</h1>
+
+      <p>You can close this tab.</p>
+    </div>
+  );
 }
 
 ReactDOM.createRoot(document.getElementById("root")).render(
@@ -15,6 +35,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
         <Route path="/" element={<App />} />
 
         <Route path="/workflow/:id" element={<App />} />
+
         <Route path="/oauth-success" element={<OAuthSuccess />} />
       </Routes>
     </BrowserRouter>
