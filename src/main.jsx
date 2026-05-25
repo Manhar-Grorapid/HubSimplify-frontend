c7c4e8f574e09418942bdeaa80cbb0fe38f1c448
manhar <manharchaurasia100@gmail.com>
2026-05-25 15:07:12 +0530

    inline main test

--- src/main.jsx
+++ src/main.jsx
@@ -17,7 +17,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
 
         <Route path="/workflow/:id" element={<App />} />
 
-        <Route path="/oauth-success" element={<OAuthSuccess />} />
+        <Route path="/oauth-success" element={<div><h1>HELLLLLLOO</h1></div>} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
