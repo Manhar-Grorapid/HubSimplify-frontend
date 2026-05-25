d3331c8bbcf42f73fd2688235e841144bfc2b97c
manhar <manharchaurasia100@gmail.com>
2026-05-25 13:38:03 +0530

    inline main test

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,88 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom/client";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import App from "./App";
 
 function OAuthSuccess() {
-  return <h1>RANDOM-928374-TEST</h1>;
+
+  useEffect(() => {
+
+    try {
+
+      const params =
+        new URLSearchParams(
+          window.location.search
+        );
+
+      const userId =
+        params.get("userId");
+
+      if (userId) {
+
+        localStorage.setItem(
+          "hubsimplify_user_id",
+          userId
+        );
+
+        console.log(
+          "USER ID STORED:",
+          userId
+        );
+      }
+
+    } catch (error) {
+
+      console.log(error);
+    }
+
+  }, []);
+
+  return (
+
+    <div>
+
+      <h1>
+        HubSpot Connected Successfully
+      </h1>
+
+      <p>
+        You can close this tab.
+      </p>
+
+    </div>
+  );
 }
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
+ReactDOM.createRoot(
+  document.getElementById("root")
+).render(
 
-        <Route path="/workflow/:id" element={<App />} />
-        <Route path="/oauth-success" element={<OAuthSuccess />} />
+  <React.StrictMode>
+
+    <BrowserRouter>
+
+      <Routes>
+
+        <Route
+          path="/"
+          element={<App />}
+        />
+
+        <Route
+          path="/workflow/:id"
+          element={<App />}
+        />
+
+        <Route
+          path="/oauth-success"
+          element={<OAuthSuccess />}
+        />
+
       </Routes>
+
     </BrowserRouter>
-  </React.StrictMode>,
+
+  </React.StrictMode>
 );
