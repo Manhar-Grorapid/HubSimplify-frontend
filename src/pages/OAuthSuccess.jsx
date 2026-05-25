71014fd7f6f376ec88679399e3db61bc520a2892
manhar <manharchaurasia100@gmail.com>
2026-05-25 17:05:54 +0530

    store userid

--- src/pages/OAuthSuccess.jsx
+++ src/pages/OAuthSuccess.jsx
@@ -1,9 +1,25 @@
 import { useEffect } from "react";
 
 export default function OAuthSuccess() {
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+
+    const userId = params.get("userId");
+
+    console.log("USER ID:", userId);
+
+    if (userId) {
+      localStorage.setItem("userId", userId);
+
+      console.log("User ID stored successfully");
+    } else {
+      console.error("No userId found in URL");
+    }
+  }, []);
+
   return (
     <div>
-      <h1>OAuth Success Working.......</h1>
+      <h1>OAuth Success Working</h1>
     </div>
   );
 }
