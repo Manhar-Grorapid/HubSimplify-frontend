6a0d4df8fd8400bc21beb838036287e8075f2e92
manhar <manharchaurasia100@gmail.com>
2026-05-25 16:47:24 +0530

    removed double render-

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App";
+import OAuthSuccess from "./pages/OAuthSuccess";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <div
-    style={{
-      color: "red",
-      fontSize: "40px",
-      background: "white",
-      padding: "50px",
-      zIndex: 999999,
-    }}
-  >
-    TEST WORKING
-  </div>,
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/workflow/:id" element={<App />} />
+
+      <Route path="/oauth-success" element={<OAuthSuccess />} />
+    </Routes>
+  </BrowserRouter>,
 );
