d4e9f599ba336479d11cde963a34208ce7014bff
manhar <manharchaurasia100@gmail.com>
2026-05-26 14:46:50 +0530

    hardcode userid for test

--- src/pages/OAuthSuccess.jsx
+++ src/pages/OAuthSuccess.jsx
@@ -4,12 +4,14 @@ export default function OAuthSuccess() {
   useEffect(() => {
     const params = new URLSearchParams(window.location.search);
 
-    const userId = params.get("userId");
+    const userId = "066e20b8-3333-486f-9b60-4fe9ac89232f";
 
     console.log("USER ID:", userId);
 
     if (userId) {
-      localStorage.setItem("userId", userId);
+      chrome.storage.local.set({
+        userId: userId,
+      });
 
       console.log("User ID stored successfully");
     } else {
