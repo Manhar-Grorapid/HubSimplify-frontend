8743c275517afd414d98f3a03de1e56bcce3ff1e
manhar <manharchaurasia100@gmail.com>
2026-05-28 11:17:31 +0530

    use hash router

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import OAuthSuccess from "./pages/OAuthSuccess.jsx";
 import "./index.css";
@@ -8,11 +8,11 @@ import "./index.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <Routes>
       <Route path="/workflow/:id" element={<App />} />
 
       <Route path="/oauth-success" element={<OAuthSuccess />} />
     </Routes>
-  </BrowserRouter>,
+  </HashRouter>,
 );
